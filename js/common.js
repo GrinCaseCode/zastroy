61264b1db51923e055caabe2fcaf444148ba5568
nickolay.grin <grinmail84@gmail.com>
2022-01-05 15:40:13 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -18,6 +18,13 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
     $menu.removeClass("fixed").addClass("default");
   }
 
+
+$(".item-question__head").click(function() {
+  $(this).parent().toggleClass("active");
+  $(this).siblings().slideToggle(200);
+  $(this).parent().siblings(".item-question").removeClass("active");
+  $(this).parent().siblings(".item-question").find(".item-question__content").slideUp(200);
+});
 	//плавный скролл
 	$(".navigat li a").mPageScroll2id();
 
